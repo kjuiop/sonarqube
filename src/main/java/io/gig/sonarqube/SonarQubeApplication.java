d8a0e2cd83b75bd45360dbe81b4b7d02bca666a3
kjuiop <copying_y@naver.com>
2023-07-12 20:23:57 +0900

    feat: springboot web application

---
+++ src/main/java/io/gig/sonarqube/SonarQubeApplication.java
@@ -0,0 +1,17 @@
+package io.gig.sonarqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : JAKE
+ * @date : 2023/07/12
+ */
+@SpringBootApplication
+public class SonarQubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonarQubeApplication.class, args);
+    }
+
+}
